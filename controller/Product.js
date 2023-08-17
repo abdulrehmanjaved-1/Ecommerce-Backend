e5091b95d3c410d706776c344261c7dcfbfd7e00
abdulrehmanjaved1 <abdulrehmanjaveds12@gmail.com>
2023-08-17 22:27:08 +0500

    Final Commit in Dev Env

--- controller/Product.js
+++ controller/Product.js
@@ -2,6 +2,9 @@ const { Product } = require("../model/Product");
 
 exports.createProduct = async (req, res) => {
   const product = new Product(req.body);
+  product.discountPrice = Math.round(
+    product.price * (1 - product.discountPercentage / 100)
+  );
   try {
     const doc = await product.save();
     res.status(201).json(doc);
@@ -10,21 +13,23 @@ exports.createProduct = async (req, res) => {
   }
 };
 exports.fetchAllProducts = async (req, res) => {
-  let condition={};
-  if(!req.query.admin){
-    condition.deleted={$ne:true};
+  let condition = {};
+  if (!req.query.admin) {
+    condition.deleted = { $ne: true };
   }
   let query = Product.find(condition);
   let totalProductsQuery = Product.find(condition);
   if (req.query.category) {
-    query = query.find({ category: req.query.category });
+    query = query.find({ category: { $in: req.query.category.split(",") } });
     totalProductsQuery = totalProductsQuery.find({
       category: req.query.category,
     });
   }
   if (req.query.brand) {
-    query = query.find({ brand: req.query.brand });
-    totalProductsQuery = totalProductsQuery.find({ brand: req.query.brand });
+    query = query.find({ brand: { $in: req.query.category.split(",") } });
+    totalProductsQuery = totalProductsQuery.find({
+      brand: { $in: req.query.category.split(",") },
+    });
   }
   if (req.query._sort && req.query._order) {
     query = query.sort({ [req.query._sort]: req.query._order });
@@ -45,22 +50,29 @@ exports.fetchAllProducts = async (req, res) => {
   }
 };
 
-exports.fetchProductById=async(req,res)=>{
-  const {id}=req.params;
+exports.fetchProductById = async (req, res) => {
+  const { id } = req.params;
   try {
-    const product=await Product.findById(id);
-    res.status(200).json(product)
+    const product = await Product.findById(id);
+    res.status(200).json(product);
   } catch (error) {
     res.status(400).json(error);
   }
-}
+};
 
-exports.updateProduct=async(req,res)=>{
-  const {id}=req.params;
+exports.updateProduct = async (req, res) => {
+  const { id } = req.params;
+
   try {
-    const product=await Product.findByIdAndUpdate(id,req.body,{new:true});
-    res.status(200).json(product)
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    product.discountPrice = Math.round(
+      product.price * (1 - product.discountPercentage / 100)
+    );
+      const updatedProduct=await product.save();
+    res.status(200).json(updatedProduct);
   } catch (error) {
     res.status(400).json(error);
   }
-}
+};
